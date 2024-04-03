aa7eda9c0d561f4ef9ef15cfb7d6d1e6229e5804
1GodLess5 <gacekalbert@seznam.cz>
2024-04-03 21:50:43 +0200

    added properties and constructor

--- src/main/java/cz/godless/domain/Clazz.java
+++ src/main/java/cz/godless/domain/Clazz.java
@@ -1,5 +1,15 @@
 package cz.godless.domain;
 
-public class Clazz {
+import java.util.List;
 
+public class Clazz {
+    private final String clazzName;
+    private final String primaryTeacher;
+    private final List<String> students;
+
+    public Clazz(String clazzName, String primaryTeacher, List<String> students) {
+        this.clazzName = clazzName;
+        this.primaryTeacher = primaryTeacher;
+        this.students = students;
+    }
 }
