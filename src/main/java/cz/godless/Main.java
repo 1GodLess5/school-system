b1ec8ffce83390c4caa4e4385cc4e8252a3ecb37
1GodLess5 <gacekalbert@seznam.cz>
2024-04-03 22:11:16 +0200

    created object firstClass and tested it

--- src/main/java/cz/godless/Main.java
+++ src/main/java/cz/godless/Main.java
@@ -1,7 +1,34 @@
 package cz.godless;
 
+import cz.godless.domain.Clazz;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        String firstClassName = "1.A";
+        String primaryTeacher = "Prokopova";
+        List<String> students = new ArrayList<>();
+        students.add("Albert Gacek");
+        students.add("Adam Vasek");
+        students.add("Lukas Byrtus");
 
+        Clazz firstClass = new Clazz(firstClassName, primaryTeacher, students);
+
+        System.out.println("Class: " + firstClass.getClazzName());
+        System.out.println("Primary teacher: " + firstClass.getPrimaryTeacher());
+        
+        System.out.print("Students: ");
+        int studentCount = 0;
+        for (String student : students) {
+            studentCount++;
+            System.out.print(student);
+            
+            if (studentCount < students.size()) {
+                System.out.print(", ");
+            }
+        }
+        
     }
 }
