c21f6410b96cd3857113de936b013307fcf91a25
1GodLess5 <gacekalbert@seznam.cz>
2024-04-04 23:10:03 +0200

    new method randomSubjectPicking(), modified constructor

--- src/main/java/cz/godless/domain/Student.java
+++ src/main/java/cz/godless/domain/Student.java
@@ -2,15 +2,19 @@ package cz.godless.domain;
 
 import cz.godless.enums.Subjects;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
 
 public class Student {
     private final String name;
     private final List<Subjects> takesSubjects;
+    private final Random random = new Random();
 
-    public Student(String name, List<Subjects> takesSubjects) {
+    public Student(String name) {
         this.name = name;
-        this.takesSubjects = takesSubjects;
+        this.takesSubjects = randomSubjectPicking();
     }
 
     public String getName() {
@@ -19,5 +23,30 @@ public class Student {
 
     public List<Subjects> getTakesSubjects() {
         return takesSubjects;
+    }
+
+    private List<Subjects> randomSubjectPicking() {
+        List<Subjects> subjects = new ArrayList<>();
+        int subjectsLength = 0;
+        for (Subjects subject : Subjects.values()) {
+            if (subject.getCount() > subjectsLength) {
+                subjectsLength = subject.getCount();
+            }
+        }
+
+        int i = 0;
+        while (i < 5) {
+            int generateInt = (int) ((Math.random() * (subjectsLength + 1)) + 0);
+            for (Subjects subject : Subjects.values()) {
+                if (subject.getCount() == generateInt) {
+                    if (!subjects.contains(subject)) {
+                        subjects.add(subject);
+                        i++;
+                    }
+                }
+            }
+        }
+
+        return subjects;
     }
 }
