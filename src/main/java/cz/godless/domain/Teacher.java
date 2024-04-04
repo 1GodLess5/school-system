5cb7b8f00bb108c749dc7d88d369d1337bca6259
1GodLess5 <gacekalbert@seznam.cz>
2024-04-04 22:03:28 +0200

    added method nameGender() -> returns changed name based on gender

--- src/main/java/cz/godless/domain/Teacher.java
+++ src/main/java/cz/godless/domain/Teacher.java
@@ -8,4 +8,9 @@ public class Teacher {
         this.name = name;
         this.isWoman = isWoman;
     }
+
+    private void nameGender() {
+        String nameBase = this.name;
+        this.name = isWoman ? "Ms. " + nameBase : "Mr. " + nameBase;
+    }
 }
