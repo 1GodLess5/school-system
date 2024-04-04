8eb1f5bcb6ceb3178e24c5bbad316feac51f4436
1GodLess5 <gacekalbert@seznam.cz>
2024-04-04 22:50:39 +0200

    added property count, added more subjects to choose from

--- src/main/java/cz/godless/enums/Subjects.java
+++ src/main/java/cz/godless/enums/Subjects.java
@@ -1,17 +1,38 @@
 package cz.godless.enums;
 
 public enum Subjects {
-    MATHEMATICS("Mathematics"),
-    ENGLISH("English"),
-    P_E("P/E");
+    MATHEMATICS("Mathematics", 0),
+    MUSIC("Music", 1),
+    SOCIAL_STUDIES("Social studies", 2),
+    DRAMA("Drama", 3),
+    COMPUTING("Computing", 4),
+    LITERATURE("Literature", 5),
+    SCIENCE("Science", 6),
+    ART("Art", 7),
+    GEOGRAPHY("Geography", 8),
+    IT("IT", 9),
+    GRAPHIC_DESIGN("Graphic design", 10),
+    PHILOSOPHY("Philosophy", 11),
+    P_E("P/E", 12),
+    HISTORY("History", 13),
+    LANGUAGES("Languages", 14),
+    BIOLOGY("Biology", 15),
+    CHEMISTRY("Chemistry", 16),
+    ENGLISH("English", 17);
 
     private final String description;
+    private final int count;
 
-    Subjects(String description) {
+    Subjects(String description, int count) {
         this.description = description;
+        this.count = count;
     }
 
     public String getDescription() {
         return description;
+    }
+
+    public int getCount() {
+        return count;
     }
 }
