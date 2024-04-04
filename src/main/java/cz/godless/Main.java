82e46294e035ceb6dd5f6ed67f057e80ca2e5b37
1GodLess5 <gacekalbert@seznam.cz>
2024-04-04 22:32:27 +0200

    modified Main to accept Student class

--- src/main/java/cz/godless/Main.java
+++ src/main/java/cz/godless/Main.java
@@ -1,6 +1,7 @@
 package cz.godless;
 
 import cz.godless.domain.Clazz;
+import cz.godless.domain.Student;
 import cz.godless.domain.Teacher;
 
 import java.util.ArrayList;
@@ -10,25 +11,28 @@ public class Main {
     public static void main(String[] args) {
         String firstClassName = "1.A";
         Teacher primaryTeacher = new Teacher("Prokopova", true);
-        List<String> students = new ArrayList<>();
-        students.add("Albert Gacek");
-        students.add("Adam Vasek");
-        students.add("Lukas Byrtus");
+
+        List<String> studentsSubjects = new ArrayList<>();
+        studentsSubjects.add("Math");
+        studentsSubjects.add("English");
+        studentsSubjects.add("P/E");
+        Student student1 = new Student("Albert Gacek", studentsSubjects);
+        Student student2 = new Student("Adam Vasek", studentsSubjects);
+        Student student3 = new Student("Lukas Byrtus", studentsSubjects);
+
+        List<Student> students = new ArrayList<>();
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
 
         Clazz firstClass = new Clazz(firstClassName, primaryTeacher, students);
 
         System.out.println("Class: " + firstClass.getClazzName());
         System.out.println("Primary teacher: " + firstClass.getPrimaryTeacher().getName());
         
-        System.out.print("Students: ");
-        int studentCount = 0;
-        for (String student : firstClass.getStudents()) {
-            studentCount++;
-            System.out.print(student);
-            
-            if (studentCount < firstClass.getStudents().size()) {
-                System.out.print(", ");
-            }
+        System.out.println("Students: ");
+        for (Student student : firstClass.getStudents()) {
+            System.out.println("\t" + student.getName() + ": " + student.getTakesSubjects());
         }
         
     }
