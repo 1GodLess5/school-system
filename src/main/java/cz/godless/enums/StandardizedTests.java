198917ed688d7a964120bedc6802b3462dac3d2b
1GodLess5 <gacekalbert@seznam.cz>
2024-04-08 21:50:40 +0200

    added test types

--- src/main/java/cz/godless/enums/StandardizedTests.java
+++ src/main/java/cz/godless/enums/StandardizedTests.java
@@ -1,7 +1,14 @@
 package cz.godless.enums;
 
 public enum StandardizedTests {
-    ;
+    DIAGNOSTIC_ASSESSMENT("Diagnostic Assessment", 0),
+    IPSATIVE_ASSESSMENT("Ipsative Assessment", 1),
+    PLACEMENT_TEST("Placement Test", 2),
+    MULTIPLE_CHOICE("Multiple Choice", 3),
+    WRITTEN_TEST("Written Test", 4),
+    ACHIEVEMENT_TEST("Achievement Test", 5),
+    ORAL_EXAMS("Oral Exams", 6),
+    SKILL_ASSESSMENT("Skill Assessment", 7);
 
     private final String description;
     private final int count;
