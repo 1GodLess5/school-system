4aa6bc0b1489bca0fbdb9daf543a0cfb777b1e20
1GodLess5 <gacekalbert@seznam.cz>
2024-04-08 21:06:06 +0200

    modified randomSubjectPicking() -> refactor of unnecessary inner function

--- src/main/java/cz/godless/utility/NumberGenerator.java
+++ src/main/java/cz/godless/utility/NumberGenerator.java
@@ -1,5 +1,10 @@
 package cz.godless.utility;
 
+import cz.godless.domain.Grade;
+import cz.godless.enums.Subjects;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class NumberGenerator {
@@ -11,5 +16,24 @@ public class NumberGenerator {
 
     public static float generateFloat(float minValueIncl, float maxValueIncl) {
         return (float) ((Math.random() * (maxValueIncl - minValueIncl + 1)) + minValueIncl);
+    }
+
+    public static List<Subjects> randomSubjectPicking() {
+        List<Subjects> subjects = new ArrayList<>();
+        int i = 0;
+
+        while (i < 5) {
+            int generatedInt = generateInt(0, Subjects.values().length - 1);
+            for (Subjects subject : Subjects.values()) {
+                if (subject.getCount() == generatedInt) {
+                    if (!subjects.contains(subject)) {
+                        subjects.add(subject);
+                        i++;
+                    }
+                }
+            }
+        }
+
+        return subjects;
     }
 }
