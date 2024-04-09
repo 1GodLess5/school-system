c64d4f1e657a480f9a9764f6cc2fd63df8bb1df7
1GodLess5 <gacekalbert@seznam.cz>
2024-04-09 19:52:04 +0200

    new interface CountableEnum -> defined abstract methods getDescription(), getCount() 			    -> implemented static method getDescriptionByCouny() which now will work for all enums which are implementing this interface

--- src/main/java/cz/godless/enums/CountableEnum.java
+++ src/main/java/cz/godless/enums/CountableEnum.java
@@ -1,25 +1,25 @@
 package cz.godless.enums;
 
-public abstract class CountableEnum {
-    private final String description;
-    private final int count;
+import java.lang.reflect.Method;
 
-    protected CountableEnum(String description, int count) {
-        this.description = description;
-        this.count = count;
-    }
+public interface CountableEnum {
+    String getDescription();
 
-    public String getDescription() {
-        return description;
-    }
+    int getCount();
 
-    public int getCount() {
-        return count;
-    }
-
-    protected String getDescriptionByCount(int count) {
-        if (count == this.getCount()) {
-            return this.getDescription();
-        } else return null;
+    static <T extends Enum<T>> String getDescriptionByCount(int count, Class<T> enumClass) {
+        for (T enumConstant : enumClass.getEnumConstants()) {
+            try {
+                Method getCountMethod = enumClass.getMethod("getCount");
+                int enumCount = (int) getCountMethod.invoke(enumConstant);
+                if (enumCount == count) {
+                    Method getDescriptionMethod = enumClass.getMethod("getDescription");
+                    return (String) getDescriptionMethod.invoke(enumConstant);
+                }
+            } catch (Exception e) {
+                System.out.println("Sorry, we have encountered an error on our side.");
+            }
+        }
+        return null;
     }
 }
