9ab155f9f4f7e4b2d5d79edc4fe70eb7a275f151
1GodLess5 <gacekalbert@seznam.cz>
2024-04-09 19:24:34 +0200

    new abstract class CountableEnum

--- src/main/java/cz/godless/enums/CountableEnum.java
+++ src/main/java/cz/godless/enums/CountableEnum.java
@@ -1,6 +1,25 @@
 package cz.godless.enums;
 
-public interface CountableEnum {
-    String getDescription();
-    int getCount();
+public abstract class CountableEnum {
+    private final String description;
+    private final int count;
+
+    protected CountableEnum(String description, int count) {
+        this.description = description;
+        this.count = count;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    protected String getDescriptionByCount(int count) {
+        if (count == this.getCount()) {
+            return this.getDescription();
+        } else return null;
+    }
 }
