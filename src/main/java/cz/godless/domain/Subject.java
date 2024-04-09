90d898269bb883add67192de2a6a11ecb4ceb85c
1GodLess5 <gacekalbert@seznam.cz>
2024-04-09 18:07:34 +0200

    added property subjectName -> added getter

--- src/main/java/cz/godless/domain/Subject.java
+++ src/main/java/cz/godless/domain/Subject.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Map;
 
 public class Subject {
+    private final String subjectName;
     private final Teacher teacherOfSubject;
     private final Map<Student, List<Grade>> takesClass;
 
-    public Subject(Teacher teacherOfSubject, Map<Student, List<Grade>> takesClass) {
+    public Subject(String subjectName, Teacher teacherOfSubject, Map<Student, List<Grade>> takesClass) {
+        this.subjectName = subjectName;
         this.teacherOfSubject = teacherOfSubject;
         this.takesClass = takesClass;
     }
@@ -18,5 +20,9 @@ public class Subject {
 
     public Map<Student, List<Grade>> getTakesClass() {
         return takesClass;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
     }
 }
