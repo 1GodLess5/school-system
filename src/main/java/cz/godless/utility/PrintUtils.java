45c7a90113ff40b00e13bd381f757f7d77a80af2
1GodLess5 <gacekalbert@seznam.cz>
2024-04-17 20:53:04 +0200

    new static method printClazz()

--- src/main/java/cz/godless/utility/PrintUtils.java
+++ src/main/java/cz/godless/utility/PrintUtils.java
@@ -12,4 +12,25 @@ public class PrintUtils {
         System.out.println(divider);
     }
 
+    public static void printClazz(Clazz clazz) {
+        printDivider(85);
+        System.out.println("Class: " + clazz.getClazzName());
+        System.out.println("Primary teacher: " + clazz.getPrimaryTeacher().getName());
+        printDivider(17 + clazz.getPrimaryTeacher().getName().length());
+        System.out.println("Students: ");
+        for (Student student : clazz.getStudents()) {
+            System.out.printf("\t%-20s -\t", student.getName());
+            int counter = 1;
+            for (String subject : student.getTakesSubjects()) {
+                if (counter + 1 > student.getTakesSubjects().size()) {
+                    System.out.print(subject);
+                } else {
+                    System.out.print(subject + ", ");
+                }
+                counter++;
+            }
+            System.out.println();
+        }
+        printDivider(85);
+    }
 }
